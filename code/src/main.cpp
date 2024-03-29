9ac91bd0759f521e11f92dafd04421396165dcc0
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2024-03-29 00:05:34 -0300

    update base code

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,6 +1,23 @@
-#include <cv2/imgproc.hpp>
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-int main()
+void webcam()
 {
+    cv::VideoCapture cap(0);
+    cv::Mat img;
+
+    while (true)
+    {
+        cap.read(img);
+        cv::imshow("Image", img);
+        cv::waitKey(1);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    webcam();
     return 0;
 }
